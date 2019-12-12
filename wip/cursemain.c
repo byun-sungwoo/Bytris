e7a685b39cf9df7888871c1569a710949d5610e2
byun-sungwoo <dbyun1999@gmail.com>
2019-12-12 14:54:14 -0500

    update

--- wip/cursemain.c
+++ wip/cursemain.c
@@ -11,14 +11,14 @@
 #include "board.c"
 #define AUTODROP 20
 #define TICKRATE 0.8
-#define HARDDROP	32
-#define ROTATERIGHT	65
-#define SOFTDROP	66
-#define MOVERIGHT	67
-#define MOVELEFT	68
-#define HOLD		99
-#define ROTATELEFT	122
-#define RESET		'r'
+#define HARDDROP	32	// spacebar
+#define ROTATERIGHT	65	// arrow up
+#define SOFTDROP	66	// arrow down
+#define MOVERIGHT	67	// arrow right
+#define MOVELEFT	68	// arrow left
+#define HOLD		'c'
+#define ROTATELEFT	'z'
+#define RESET		'a'
 
 // Implemented:
 // Gamemode		: Sprint
@@ -38,7 +38,7 @@
 // Input		: Add DAS/ARR
 
 int dimr,dimc;
-void wdrawblock(WINDOW*,int,int,int);
+void wdrawblock(WINDOW*,int,int);
 void wdrawhold(WINDOW*,int,int);
 void sprint(int);
 double seconds(clock_t);
@@ -95,11 +95,15 @@ void sprint(int goal) {
 	clock_t timer,resetoffset,liveoffset,inputoffset;
 	resetoffset = 0;
 	int lockdelaycap = 5;
-	int lockdelaycounter = 0;
-	int tickcounter = 0;
+	int lockdelaycounter;
+	int tickcounter;
 	int oldboard[HEIGHT][WIDTH];
 	// start up tetris
 	NEWGAME:
+	liveoffset = 0;
+	inputoffset = 0;
+	lockdelaycounter = 0;
+	tickcounter = 0;
 	clear();
 	wclear(well);
 	wclear(stats);
@@ -118,65 +122,52 @@ void sprint(int goal) {
 	// draw game screen
 	for(i=0;i<HEIGHT;i++)
 		for(j=0;j<WIDTH;j++)
-			wdrawblock(well,i,j,0);
+			wdrawblock(well,i,j);
 	wrefresh(well);
 	wrefresh(stats);
 	wrefresh(whold);
 	wrefresh(wnext);
 	wrefresh(message);
-	liveoffset = 0;
-	inputoffset = 0;
-	lockdelaycounter = 0;
-	tickcounter = 0;
 	memcpy(oldboard,board,sizeof(int)*HEIGHT*WIDTH);
 	while(!gameover) {
 		// update timers
 		timer = clock()-resetoffset;
-		if(seconds(timer-liveoffset) > AUTODROP) {	// autodrop
+		if(seconds(timer-liveoffset) > AUTODROP) {
+			// autodrop case
 			harddroplive();
 			tickcounter = 0;
-			liveoffset = clock();
+			liveoffset = clock()-resetoffset;
 			inputoffset = clock();
-		} else {			// user input
+		} else {
+			// user input
 			input = getch();
-			if(input == SOFTDROP) {
-				int check = shiftlive(1,0);
-				if(check == 1)
+			if(input == SOFTDROP)
+				if(shiftlive(1,0) == 1)
 					inputoffset = clock();
-			}
-			if(input == HARDDROP) {
-				harddroplive();
-				tickcounter = 0;
-				liveoffset = clock();
+			if(	input == HARDDROP || input == MOVELEFT || input == MOVERIGHT ||
+				input == ROTATELEFT || input == ROTATERIGHT) {
 				inputoffset = clock();
-			}
-			if(input == MOVELEFT) {
-				shiftlive(0,-1);
-				inputoffset = clock();
-			}
-			if(input == MOVERIGHT) {
-				shiftlive(0,1);
-				inputoffset = clock();
-			}
-			if(input == ROTATELEFT) {
-				rotatelive(0);
-				inputoffset = clock();
-			}
-			if(input == ROTATERIGHT) {
-				rotatelive(1);
-				inputoffset = clock();
-			}
-			if(input == HOLD) {
-				//holdlive();
-				//tickcounter = 0;
-				//liveoffset = clock();
-				//inputoffset = clock();
+				if(input == HARDDROP) {
+					harddroplive();
+					tickcounter = 0;
+					liveoffset = clock()-resetoffset;
+				}
+				if(input == HOLD) {
+					holdlive();
+					tickcounter = 0;
+					liveoffset = clock()-resetoffset;
+				}
+				if(input == MOVELEFT) shiftlive(0,-1);
+				if(input == MOVERIGHT) shiftlive(0,1);
+				if(input == ROTATELEFT) rotatelive(0);
+				if(input == ROTATERIGHT) rotatelive(1);
 			}
 			if(input == RESET) {
 				resetoffset = clock();
 				goto NEWGAME;
 			}
 			// downtick
+			mvprintw(6,0,"%f > %f", seconds(timer-liveoffset), TICKRATE*(tickcounter+1));
 			if(seconds(timer-liveoffset) > TICKRATE*(tickcounter+1)) {
 				tickcounter++;
 				int atfloor = !shiftlive(1,0);
@@ -185,19 +176,14 @@ void sprint(int goal) {
 					(atfloor && seconds(timer-inputoffset)>TICKRATE)) {
 					harddroplive();
 					tickcounter = 0;
-					liveoffset = clock();
+					liveoffset = clock()-resetoffset;
 				}
 			}
 		}
 		// update well visual
-		int ghosttracker = 0;
 		for(i=0;i<HEIGHT;i++)
 			for(j=0;j<WIDTH;j++)
-				if(1 || board[i][j] != oldboard[i][j] || (board[i][j] == 1 && i<2)) {
-					if(board[i][j] == 1)
-						ghosttracker++;
-					wdrawblock(well,i,j,ghosttracker);
-				}
+				wdrawblock(well,i,j);
 		// data well visual
 		for(i=1;i<HEIGHT;i++)
 			for(j=0;j<WIDTH;j++)
@@ -205,7 +191,7 @@ void sprint(int goal) {
 		// update stat visual
 		mvwprintw(stats,1,2,"%.3fs",seconds(timer));
 		mvwprintw(stats,2,2,"%d/%d",linescleared,goal);
-		// update whold visual
+		// update hold visual
 		for(i=0;i<hold.size.row;i++)
 			for(j=0;j<hold.size.col;j++) {
 				if(hold.data[i][j] == 1) {
@@ -215,7 +201,7 @@ void sprint(int goal) {
 				} else
 					mvwprintw(whold,i+1,1+j*2,"  ");
 			}
-		// update next visual
+		// update queue visual
 		for(k=0;k<5;k++) {
 			tetromino t = next[k];
 			for(i=0;i<MAX-1;i++) {
@@ -239,9 +225,9 @@ void sprint(int goal) {
 		// check goal reached
 		if(linescleared >= goal)
 			break;
-		mvprintw(0,20,"holdstatus:%d",holdstatus);
-		mvprintw(1,20,"holdcolor:%d",hold.color);
-		mvprintw(2,20,"livecolor:%d",live.color);
+		mvprintw(1,0,"holdstatus:%d",holdstatus);
+		mvprintw(2,0,"holdcolor:%d",hold.color);
+		mvprintw(3,0,"livecolor:%d",live.color);
 	}
 	if(!gameover)
 		mvwprintw(message,1,2,"nice");
@@ -252,7 +238,7 @@ void sprint(int goal) {
 	getch();
 }
 
-void wdrawblock(WINDOW *win, int row, int col, int ghosttracker) {
+void wdrawblock(WINDOW *win, int row, int col) {
 	if(row >= 1 && row < HEIGHT && col >= 0 && col < WIDTH) {
 		int current = board[row][col];
 		int scry = row;

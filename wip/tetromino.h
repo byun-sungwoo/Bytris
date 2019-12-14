ae523987dcf849ecae65598907d5d495cc2701f4
Daniel Byun <dsbyun@iu.edu>
2019-12-14 02:06:28 -0500

    bytris: works in iu ssh

--- wip/tetromino.h
+++ wip/tetromino.h
@@ -8,11 +8,11 @@ typedef struct rowcol {
 	int col;
 } rowcol;
 
-const rowcol spawn = {1,4};
+//const rowcol {1,4} = {1,4};
 
 typedef struct tetromino {
 	int mode;		// rotation mode: 0 normal, 1 offset
-	int state;		// block state	: 0 spawn, 1 right, 2 180, 3 left
+	int state;		// block state	: 0 {1,4}, 1 right, 2 180, 3 left
 	rowcol size;		// size, 1 index
 	rowcol pivot;		// pivot point, 0 index
 	rowcol pos;		// position on board, 0 index
@@ -20,12 +20,12 @@ typedef struct tetromino {
 	int data[MAX][MAX];	// tetris block data
 } tetromino;
 
-const tetromino pool[7] = {	{0,0,{2,3},{1,1},spawn,3,{{0,1,1},{1,1,0}}},	// s
-				{0,0,{2,3},{1,1},spawn,4,{{1,1,0},{0,1,1}}},	// z
-				{0,0,{2,3},{1,1},spawn,5,{{0,0,1},{1,1,1}}},	// l
-				{0,0,{2,3},{1,1},spawn,6,{{1,0,0},{1,1,1}}},	// j
-				{0,0,{2,3},{1,1},spawn,7,{{0,1,0},{1,1,1}}},	// t
-				{1,0,{1,4},{0,1},spawn,8,{{1,1,1,1}}},		// i
+const tetromino pool[7] = {	{0,0,{2,3},{1,1},{1,4},3,{{0,1,1},{1,1,0}}},	// s
+				{0,0,{2,3},{1,1},{1,4},4,{{1,1,0},{0,1,1}}},	// z
+				{0,0,{2,3},{1,1},{1,4},5,{{0,0,1},{1,1,1}}},	// l
+				{0,0,{2,3},{1,1},{1,4},6,{{1,0,0},{1,1,1}}},	// j
+				{0,0,{2,3},{1,1},{1,4},7,{{0,1,0},{1,1,1}}},	// t
+				{1,0,{1,4},{0,1},{1,4},8,{{1,1,1,1}}},		// i
 				{1,0,{2,2},{0,0},{0,4},9,{{1,1},{1,1}}}};	// o
 
 void setrowcol(rowcol*,int,int);
